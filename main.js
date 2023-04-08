4657b0100162bcf5cdd7cc13151539ed91268094
JulienDegermann <degermann.julien@gmail.com>
2023-04-08 16:19:30 +0200

    add conditions for email format and passord.lenth

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const lastname = document.querySelector('#last-name') // last-name input
 const email = document.querySelector('#email') // email input
 const password = document.querySelector('#last-password') // password input
 
+// check email format input
+
 
 
 // control if input.values are not empty
@@ -20,26 +22,51 @@ inputAll.forEach((input) => {
     error.classList.add('error-message') // add .error-message class for css style
     input.parentNode.append(error) // add the new <p> in HTML after last child's element parent (after this input)
 
+    input.addEventListener('focus', () => {
+        input.target.style.border = 'solid green 1px'
+    })
+
     // add ! mark in input if error
     let errorMark = document.createElement('p') // create div for adding the mark
     errorMark.classList.add('error-mark') // add .error-mark class for css style
     // console.log(errorMark)
     // input.parentNode.append(errorMark)
     input.parentNode.append(errorMark)
+    errorMark.innerText = '!'
     errorMark.style.visibility = 'hidden'
 
 
+
+// function to check email format input
+const checkMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+
+    // regexMAil.test(input.value) === 'false
 
     // when submit, add error-message content forEach input that has empty value
     form.addEventListener('submit', (e) => { // add event listener here because can't link errorMessage to external listener
         e.preventDefault()
         if (!input.value) { // if there is no value in the input
-            errorMark.innerText = '!'
             errorMark.style.visibility = 'visible'
             error.innerText = `"${input.name}" cannot be empty`// add content in error message
+            input.style.borderColor = 'hsl(0, 100%, 74%)'
+        } else if (input.id === 'email' && !(input.value.match(checkMail))) {
+            errorMark.style.visibility = 'visible'
+            error.innerText = `Looks like this is not an email`// add content in error message
+            input.style.color = 'hsl(0, 100%, 74%)'
+            input.style.borderColor = 'hsl(0, 100%, 74%)'
+        } else if (input.id === 'password' && input.value.length < 6) {
+            errorMark.style.visibility = 'visible'
+            error.innerText = `Looks like your password is too short`// add content in error message
+            input.style.color = 'hsl(0, 100%, 74%)'
+            input.style.borderColor = 'hsl(0, 100%, 74%)'
         } else {
             error.innerText = `` // else return erro message empty
             errorMark.style.visibility = 'hidden'
+            input.style.borderColor = ''
+            input.style.color = ''
+
         }
     })
 })
+
+
